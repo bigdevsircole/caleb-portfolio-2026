8516ad3c0ade7272b085029d488a6794ccad5e59
Caleb Yinusa <caleb.yinusa@gmail.com>
2026-03-25 11:24:07 +0000

    the images from google drive are not displaying

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: '**.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
